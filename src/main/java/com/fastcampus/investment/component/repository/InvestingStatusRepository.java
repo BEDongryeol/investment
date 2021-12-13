fa2056675d5ba0135668dde8826c99d24ab54e2d
BEDongryeol <ehdfuf53@gmail.com>
2021-12-13 13:55:41 +0900

    Refactor: Add @Repository annotation

--- src/main/java/com/fastcampus/investment/component/repository/InvestingStatusRepository.java
+++ src/main/java/com/fastcampus/investment/component/repository/InvestingStatusRepository.java
@@ -2,7 +2,9 @@ package com.fastcampus.investment.component.repository;
 
 import com.fastcampus.investment.component.entity.InvestingStatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvestingStatusRepository extends JpaRepository<InvestingStatusEntity, Long> {
     InvestingStatusEntity findByUserIdAndProductsId(Long userId, Long productsId);
 
