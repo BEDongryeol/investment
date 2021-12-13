eef4b2fad06bd5013a1ed500c050f976bca64d6b
BEDongryeol <ehdfuf53@gmail.com>
2021-12-13 14:29:40 +0900

    Refactor: Delete unnecessary method

--- src/main/java/com/fastcampus/investment/component/repository/ProductsRepository.java
+++ src/main/java/com/fastcampus/investment/component/repository/ProductsRepository.java
@@ -4,14 +4,10 @@ import com.fastcampus.investment.component.entity.ProductsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<ProductsEntity, Long> {
-
-    List<ProductsEntity> findByStartedAtLessThanEqualAndFinishedAtGreaterThanEqual(LocalDate compareWithStartedAt, LocalDate compareWithFinishedAt);
 
     @Query (value = "select P FROM ProductsEntity P " +
                     "where startedAt <= current_date and finishedAt >= current_date ")
