10cdbb33b29a18f14e5d79a4712ae38a9b8f9a13
BEDongryeol <ehdfuf53@gmail.com>
2021-12-13 13:56:49 +0900

    Refactor: Add method for readability
    Changed findByStartedAtLessThanEqualAndFinishedAtGreaterThanEqual(LocalDate compareWithStartedAt, LocalDate compareWithFinishedAt) into getValidProducts()

--- src/main/java/com/fastcampus/investment/component/repository/ProductsRepository.java
+++ src/main/java/com/fastcampus/investment/component/repository/ProductsRepository.java
@@ -2,6 +2,7 @@ package com.fastcampus.investment.component.repository;
 
 import com.fastcampus.investment.component.entity.ProductsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,5 +10,12 @@ import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<ProductsEntity, Long> {
+
     List<ProductsEntity> findByStartedAtLessThanEqualAndFinishedAtGreaterThanEqual(LocalDate compareWithStartedAt, LocalDate compareWithFinishedAt);
+
+    @Query (value = "select P FROM ProductsEntity P " +
+                    "where startedAt <= current_date and finishedAt >= current_date ")
+    List<ProductsEntity> getValidProducts();
+
+
 }
